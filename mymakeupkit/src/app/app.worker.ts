07fe48d5be8710bc4804aca666452dc873681903
Amelie Ender <amelie.ender@gmx.de>
2021-09-27 20:15:04 +0200

    Fix links

--- mymakeupkit/src/app/app.worker.ts
+++ mymakeupkit/src/app/app.worker.ts
@@ -1,9 +1,7 @@
 /// <reference lib="webworker" />
 import {addData, deleteData, updateData} from './shared/dexie-db';
-// self.importScripts('./shared/dexie-db');
 
 addEventListener('message', ({ data }) => {
-  console.log(data)
   if (data.name === 'addData') {
     addData(data.data);
   } else if (data.name === 'updateDataSingle') {
